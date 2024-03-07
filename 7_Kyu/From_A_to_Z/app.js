48a7a6fcf673b2c6766c7aebf95c12203002bb98
Kim La <ktla3223@gmail.com>
2024-03-07 11:35:49 -0800

    KL. Created a function which takes a string and returns a string which includes all the letters in that range, including the last letter. Capitalization matters!

---
+++ 7_Kyu/From_A_to_Z/app.js
@@ -0,0 +1,10 @@
+function gimmeTheLetters(sp) {
+    let [start, end] = sp.split("-");
+    let startValue = start.CharCodeAt(0);
+    let endValue = end.CharCodeAt(0);
+    let result = "";
+    for(let i = startValue; i <= endValue; i++) {
+        result += String.fromCharCode(i)
+    }
+    return result;
+}
