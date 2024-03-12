3607daea3ecbef4cb653b75468859b9853c02c72
Kim La <ktla3223@gmail.com>
2024-03-12 11:35:34 -0700

    KL. Created htmls keleton and linked JS. Created a function in JS which uses return to give answers to simple arithmetics

---
+++ 8_Kyu/Fundamentals_Return/app.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    return parseFloat(a + b)
+ }
+ 
+ function divide(a,b){
+     return parseFloat( a / b);
+ }
+ 
+ function multiply(a,b){
+     return parseFloat(a * b)
+ }
+ 
+ function mod(a,b){
+     return parseFloat(a % b)
+ }
+    
+ function exponent(a,b){
+     return parseFloat(Math.pow(a, b))
+ }
+     
+ function subt(a,b){
+     return parseFloat(a - b)
+ }
