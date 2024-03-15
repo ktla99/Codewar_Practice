bd4a4c940e95ed2e0dc023ad39fb7fbb5ec03d05
Kim La <ktla3223@gmail.com>
2024-03-15 14:41:37 -0700

    KL. Created a function which takes in a string and determines if the number of o and x matches, which would return 'true' or else return 'false'

---
+++ 7_Kyu/Exes_and_Ohs/app.js
@@ -0,0 +1,14 @@
+function XO(str) {
+    let countOne = 0;
+    let countTwo = 0;
+    const lowerCase = str.toLowerCase();
+
+    for(let i = 0; i < lowerCase.length; i++) {
+        if(lowerCase[i] === "o") {
+            countOne++;
+        } else if(lowerCase[i] === "x") {
+            countTwo++;
+        }
+    }
+    return countOne === countTwo
+}
