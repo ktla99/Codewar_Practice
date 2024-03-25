9c31b73771a988a973a837f52cfca8129f25c5e4
Kim La <ktla3223@gmail.com>
2024-03-25 11:17:44 -0700

    KL. Created a function which returns complementary strands of DNA. A and T are complementary and C and G are complementary

--- 7_Kyu/Complementary_DNA/app.js
+++ 7_Kyu/Complementary_DNA/app.js
@@ -1,3 +1,16 @@
 function dnaStrand(dna){
-    
-}
+    let arr = [];
+    let convertToString = dna.split("");
+    for(let i = 0; i< convertToString.length; i++) {
+      if(convertToString[i] === "A") {
+        arr.push("T")
+      } else if(convertToString[i] === "T") {
+        arr.push("A")
+      } else if (convertToString[i] === "C") {
+        arr.push("G")
+      } else if (convertToString[i] === "G") {
+        arr.push("C")
+      }
+    }
+    return arr.join("")
+  }
