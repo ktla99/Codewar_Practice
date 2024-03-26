5085b9db79e1669a694cd5aec8be698c299ffd69
Kim La <ktla3223@gmail.com>
2024-03-26 15:36:07 -0700

    KL. Created a function which returns the first two letters of every word in an array if the array's length is an even number. If the array length is an odd number, return the same result except the last value is only returned as the first letter of the word concatenated with '_'

---
+++ 6_Kyu/Split_Strings/app.js
@@ -0,0 +1,27 @@
+function solution(str){
+    let arr = [];
+    let toArr = str.split("");
+    // console.log(str.length);
+    if(toArr.length % 2 === 0) {
+      for(let i = 0; i < toArr.length; i+=2) {
+        arr.push(toArr[i] += toArr[i+1])
+      } return arr
+    } else {
+      for(let i = 0; i < toArr.length; i+=2) {
+        arr.push(toArr[i] += toArr[i+1])
+    }
+      // find last value in an odd array
+      let lastValue = arr[arr.length-1];    
+      // turn the last value into an array of its letters
+      let splitValue = lastValue.split("");    
+      // combine the first letter of the array with '_'
+      let combineValue = splitValue[0] + "_";    
+      // transform the new string into an array
+      let changeToArr = combineValue.split();
+      // remove the last word from the array 'arr'
+      let removeLast = arr.pop();
+      // combine the new array 'arr' and array 'changeToArr'
+      let result = arr.concat(changeToArr)
+      return result
+    } 
+  }
