688199cf8152cc82e9c5912b02ace44d597558f8
Kim La <ktla3223@gmail.com>
2024-03-26 15:37:15 -0700

    KL. Placed a note on a simplified version of the function

--- 6_Kyu/Split_Strings/app.js
+++ 6_Kyu/Split_Strings/app.js
@@ -31,3 +31,19 @@ function solution(str){
 }
 
 */
+
+/* 
+function solution(str){
+   let result = []
+
+   for (let i = 0; i < str.length; i += 2) {
+     if (i < str.length - 1) {
+       result.push(str[i] + str[i + 1])
+     } else {
+      result.push(str[i] + '_') 
+     }
+   }
+   
+   return result
+}
+*/
