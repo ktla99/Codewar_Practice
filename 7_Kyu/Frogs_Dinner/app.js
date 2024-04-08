88d0f659f6d2a41b942be5d4efb71b3ba2fab4bf
Kim La <ktla3223@gmail.com>
2024-04-08 12:19:26 -0700

    KL. Created a function to determine the sumation of all the flies that has been eaten

---
+++ 7_Kyu/Frogs_Dinner/app.js
@@ -0,0 +1,39 @@
+//Return the string in the description with the number of flies eaten for each frog.
+function frogContest(n) {
+    const chris = (n * (n + 1)) / 2;
+    const tom = Math.floor(chris / 2) * (Math.floor(chris / 2) + 1) / 2;
+    const cat = (chris + tom) * ((chris + tom) + 1) / 2;
+  
+    return `Chris ate ${chris} flies, Tom ate ${tom} flies and Cat ate ${cat} flies`
+}
+
+
+/* 
+function frogContest(n) {
+  let chris = [];
+  let tom = [];
+  let cat = [];
+  for(let i = 1; i < n + 1; i++) {
+    chris.push(i)
+  }
+
+  chris = chris.reduce((acc, curr) => acc + curr, 0,)
+  console.log(chris)
+  
+  let tomInitial = Math.floor(chris / 2);
+  for(let i = 1; i < tomInitial + 1; i++) {
+    tom.push(i)
+  }
+  
+  tom = tom.reduce((acc, curr) => acc + curr, 0,)
+  console.log(tom)
+  
+  let catInitial = chris + tom
+  for(let i = 1; i < catInitial + 1; i++) {
+    cat.push(i)
+  }
+  cat = cat.reduce((acc, curr) => acc + curr, 0,)
+  
+  return `Chris ate ${chris} flies, Tom ate ${tom} flies and Cat ate ${cat} flies`
+}
+*/
